ec19b2b2b69c218d1116f7d935fbe9b5d6d6133c
felya_97@mail.ru <felya_97@mail.ru>
2022-01-22 22:39:49 +0600

    grant permissions to USERs , ADMINs

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -7,8 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/")
     public String Boom(){
-        return("<h1>Weolcome</h1>");
+        return("<h1>Weolcome bombom</h1>");
     }
-
+    @GetMapping("/user")
+    public String user(){
+        return("<h1>Weolcome User</h1>");
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return("<h1>Weolcome Admin</h1>");
+    }
 
 }
